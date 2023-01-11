45b7e6c959645daeb5c6222d0f4682f9cd4bb863
akatcha <akatcha@flash.net>
2023-01-10 23:26:31 -0600

    mimick style (remove semi-colons)

--- app.js
+++ app.js
@@ -30,18 +30,18 @@ const clearHighlight = () => {
 }
 
 const updateVictoryBanner = ({shouldShowPlayer1, shouldShowPlayer2}) => {
-    document.getElementById('player-1-wins').hidden = !shouldShowPlayer1;
-    document.getElementById('player-2-wins').hidden = !shouldShowPlayer2;
+    document.getElementById('player-1-wins').hidden = !shouldShowPlayer1
+    document.getElementById('player-2-wins').hidden = !shouldShowPlayer2
 }
 
 const clear = () => {
     const dots = document.getElementsByClassName('dot')
 
     Array.from(dots).forEach((dot) => {
-        dot.style.backgroundColor = '';
-        dot.style.border = '';
+        dot.style.backgroundColor = ''
+        dot.style.border = ''
     })
-    updateVictoryBanner({shouldShowPlayer1: false, shouldShowPlayer2: false});
+    updateVictoryBanner({shouldShowPlayer1: false, shouldShowPlayer2: false})
 }
 
 const highlight = (items) => {
@@ -252,11 +252,11 @@ const checkConnection = (startLocation) => {
         endGame()
 
         if (checkerColor === PLAYER1) {
-            updateVictoryBanner({shouldShowPlayer1: true, shouldShowPlayer2: false});
+            updateVictoryBanner({shouldShowPlayer1: true, shouldShowPlayer2: false})
 
         }
         else {
-            updateVictoryBanner({shouldShowPlayer1: false, shouldShowPlayer2: true});
+            updateVictoryBanner({shouldShowPlayer1: false, shouldShowPlayer2: true})
         }
 
     }
